91e44979a6157f4fff0d7841b81c66170ea84941
zombachu <zombachu@gmail.com>
2016-08-14 18:19:09 -1000

    Initial commit

---
+++ src/main/java/com/zombachu/afkwatch/AFKWatch.java
@@ -0,0 +1,16 @@
+package com.zombachu.afkwatch;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AFKWatch extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
