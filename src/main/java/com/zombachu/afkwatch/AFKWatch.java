8f13a72693384b28ab32643a45bf4c698619289b
zombachu <zombachu@gmail.com>
2016-08-15 16:55:02 -1000

    Cleaned up code and started the poller.

--- src/main/java/com/zombachu/afkwatch/AFKWatch.java
+++ src/main/java/com/zombachu/afkwatch/AFKWatch.java
@@ -1,12 +1,13 @@
 package com.zombachu.afkwatch;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AFKWatch extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        Bukkit.getScheduler().runTaskTimer(this, new Poller(), 0, 40);
     }
 
     @Override
